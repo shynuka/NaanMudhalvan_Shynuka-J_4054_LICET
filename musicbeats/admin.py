b665c4f476c2bf559f42cca87a96bc91cb9e9adb
shynuka <Shynuka.25cs@licet.ac.in>
2024-04-08 21:22:59 +0530

    lfs initialized

--- musicbeats/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Song, Watchlater, History, Channel
-
-# Register your models here.
-
-admin.site.register(Song)
-admin.site.register(Watchlater)
-admin.site.register(History)
-admin.site.register(Channel)
--- musicbeats/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicbeatsConfig(AppConfig):
-    name = 'musicbeats'
